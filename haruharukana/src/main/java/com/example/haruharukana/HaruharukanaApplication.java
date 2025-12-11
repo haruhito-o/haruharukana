20ba7513c6dc2b04bd83fa193da40c04b0f5481e
haruhito-o <spi1ele1kogoshi0@icloud.com>
2025-12-11 13:54:12 +0900

    データベース作成

---
+++ haruharukana/src/main/java/com/example/haruharukana/HaruharukanaApplication.java
@@ -0,0 +1,13 @@
+package com.example.haruharukana;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HaruharukanaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HaruharukanaApplication.class, args);
+	}
+
+}
